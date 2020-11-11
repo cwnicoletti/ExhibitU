21b9525e081ccb44689eb16e4479f251d7b6e91a
christiannicoletti <cwnicoletti@gmail.com>
2020-11-11 12:39:19 -0800

    Adding dummy data for testing

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -4,12 +4,14 @@ const PROJECTS = [
   new Project(
     "p1",
     "u1",
+    "u1",
     "Red Shirt",
     "https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg",
     "A red t-shirt, perfect for days with non-red weather."
   ),
   new Project(
     "p2",
+    "u1",
     "u1",
     "Blue Carpet",
     "https://images.pexels.com/photos/6292/blue-pattern-texture-macro.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
@@ -18,6 +20,7 @@ const PROJECTS = [
   new Project(
     "p3",
     "u2",
+    "u2",
     "Coffee Mug",
     "https://images.pexels.com/photos/160834/coffee-cup-and-saucer-black-coffee-loose-coffee-beans-160834.jpeg?cs=srgb&dl=bean-beans-black-coffee-160834.jpg&fm=jpg",
     "Can also be used for tea!"
@@ -25,12 +28,14 @@ const PROJECTS = [
   new Project(
     "p4",
     "u3",
-    "The Book - Limited Edition",
+    "u3",
+    "The Book - Limited Editionnnnnnnnnnnnnnnnnnnnnnnnnnnnnn",
     "https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?cs=srgb&dl=blur-blurred-book-pages-46274.jpg&fm=jpg",
     "What the content is? Why would that matter? It's a limited edition!"
   ),
   new Project(
     "p5",
+    "u3",
     "u3",
     "PowerBook",
     "https://get.pxhere.com/photo/laptop-computer-macbook-mac-screen-water-board-keyboard-technology-air-mouse-photo-airport-aircraft-tablet-aviation-office-black-monitor-keys-graphic-hardware-image-pc-exhibition-multimedia-calculator-vector-water-cooling-floppy-disk-phased-out-desktop-computer-netbook-personal-computer-computer-monitor-electronic-device-computer-hardware-display-device-448748.jpg",
@@ -38,6 +43,7 @@ const PROJECTS = [
   ),
   new Project(
     "p6",
+    "u1",
     "u1",
     "Pen & Paper",
     "https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg",
