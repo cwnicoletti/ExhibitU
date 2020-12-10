9ed5ae22f4d22c215e319e853e2ebdf7b89ccf22
christiannicoletti <cwnicoletti@gmail.com>
2020-12-09 18:15:22 -0800

    Updating explore screen

--- screens/explore/ExploreScreen.js
+++ screens/explore/ExploreScreen.js
@@ -6,57 +6,145 @@ import {
   Image,
   TouchableWithoutFeedback,
   Keyboard,
+  FlatList,
 } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { SearchBar } from "react-native-elements";
+import algoliasearch from "algoliasearch";
 
 import HeaderButton from "../../components/UI/IoniconsHeaderButton";
+import UserExploreCard from "../../components/explore/UserExploreCard";
 
 const ExploreScreen = (props) => {
   const darkModeValue = useSelector((state) => state.switches.darkMode);
-
-  const [index, setIndex] = useState("");
+  const [returnedIndex, setReturnedIndex] = useState([]);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
     props.navigation.setParams({ darkMode: darkModeValue });
   }, [darkModeValue]);
 
+  const client = algoliasearch(
+    "EXC8LH5MAX",
+    "2d8cedcaab4cb2b351e90679963fbd92"
+  );
+  const index = client.initIndex("users");
+
   const searchFilterFunction = (text) => {
     setSearch(text);
+    if (text) {
+      return index.search(text).then((responses) => {
+        console.log(responses.hits);
+        setReturnedIndex(responses.hits);
+      });
+    } else {
+      setReturnedIndex([]);
+    }
+  };
+
+  const viewProjectHandler = (
+    fullname,
+    username,
+    jobTitle,
+    resumeLinkUrl,
+    profileBiography,
+    numberOfFollowers,
+    numberOfFollowing,
+    numberOfAdvocates,
+    showResumeValue,
+    followers,
+    following,
+    advocates,
+    profileProjects
+  ) => {
+    props.navigation.navigate("ExploreProfile", {
+      fullname: fullname,
+      username: username,
+      jobTitle: jobTitle,
+      resumeLinkUrl: resumeLinkUrl,
+      profileBiography: profileBiography,
+      numberOfFollowers: numberOfFollowers,
+      numberOfFollowing: numberOfFollowing,
+      numberOfAdvocates: numberOfAdvocates,
+      showResumeValue: showResumeValue,
+      followers: followers,
+      following: following,
+      advocates: advocates,
+      profileProjects: profileProjects,
+    });
   };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View
-        style={{
-          ...styles.screen,
-          backgroundColor: darkModeValue ? "black" : "white",
-        }}
-      >
-        <SearchBar
-          containerStyle={{
-            backgroundColor: darkModeValue ? "black" : "white",
-            margin: 5,
-            borderBottomWidth: 0,
-            borderTopWidth: 0,
-            width: "80%",
-          }}
-          inputContainerStyle={{
-            height: 30,
-            backgroundColor: darkModeValue ? "black" : "white",
-            borderBottomColor: "gray",
-            borderBottomWidth: 1,
-          }}
-          searchIcon={{ size: 24 }}
-          onChangeText={(text) => searchFilterFunction(text)}
-          onClear={() => searchFilterFunction("")}
-          placeholder="Search..."
-          value={search}
-        />
-      </View>
-    </TouchableWithoutFeedback>
+    <View
+      style={{
+        ...styles.screen,
+        backgroundColor: darkModeValue ? "black" : "white",
+      }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={{ alignItems: "center" }}>
+          <SearchBar
+            containerStyle={{
+              backgroundColor: 'darkModeValue ? "black" : "white"',
+              margin: 5,
+              borderBottomWidth: 0,
+              borderTopWidth: 0,
+              width: "80%",
+            }}
+            inputContainerStyle={{
+              height: 30,
+              backgroundColor: darkModeValue ? "black" : "white",
+              borderBottomColor: "gray",
+              borderBottomWidth: 1,
+            }}
+            searchIcon={{ size: 24 }}
+            onChangeText={(text) => searchFilterFunction(text)}
+            onClear={() => {
+              searchFilterFunction("");
+            }}
+            placeholder="Search..."
+            value={search}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <FlatList
+        data={returnedIndex}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        renderItem={(itemData) => (
+          <UserExploreCard
+            image={require("../../assets/me.png")}
+            fullname={itemData.item.fullname}
+            username={itemData.item.username}
+            projectContainer={{
+              backgroundColor: darkModeValue ? "black" : "white",
+              borderColor: darkModeValue ? "gray" : "#c9c9c9",
+            }}
+            fullNameStyle={{
+              color: darkModeValue ? "white" : "black",
+            }}
+            onSelect={() => {
+              viewProjectHandler(
+                itemData.item.fullname,
+                itemData.item.username,
+                itemData.item.jobTitle,
+                itemData.item.resumeLinkUrl,
+                itemData.item.profileBiography,
+                itemData.item.numberOfFollowers,
+                itemData.item.numberOfFollowing,
+                itemData.item.numberOfAdvocates,
+                itemData.item.showResumeValue,
+                itemData.item.followers,
+                itemData.item.following,
+                itemData.item.advocates,
+                itemData.item.profileProjects
+              );
+            }}
+          />
+        )}
+      />
+    </View>
   );
 };
 
@@ -126,7 +214,6 @@ ExploreScreen.navigationOptions = (navData) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    alignItems: "center",
   },
   text: {
     padding: 10,
