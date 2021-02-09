553a08ff9c6a47cf82cb0f21a7ceb6f6260a629a
christiannicoletti <cwnicoletti@gmail.com>
2021-02-08 19:06:11 -0800

    Switching out dummy projects reducer for signup reducer

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import ReduxThunk from "redux-thunk";
 import * as Notifications from "expo-notifications";
 
-import projectsReducer from "./store/reducers/projects";
 import authReducer from "./store/reducers/auth";
 import switchesReducer from "./store/reducers/switches";
 import userReducer from "./store/reducers/user";
+import signupReducer from "./store/reducers/signup";
 
 import NavigationContainer from "./navigation/NavigationContainer";
 
-// Clapping Icon made by Freepik (https://www.flaticon.com/authors/freepik)
+// Clapping, default project icon made by Freepik (https://www.flaticon.com/authors/freepik)
 
 Notifications.setNotificationHandler({
   handleNotification: async () => {
@@ -24,7 +24,7 @@ Notifications.setNotificationHandler({
 
 const rootReducer = combineReducers({
   switches: switchesReducer,
-  projects: projectsReducer,
+  signup: signupReducer,
   auth: authReducer,
   user: userReducer,
 });
