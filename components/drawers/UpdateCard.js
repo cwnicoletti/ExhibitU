38cb5298128c064363e5fa898bc6d331cccc6f38
christiannicoletti <cwnicoletti@gmail.com>
2021-02-09 18:33:17 -0800

    Adding a "future updates" tab to right drawer

---
+++ components/drawers/UpdateCard.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
+
+import Card from "../UI/Card";
+
+const UpdateCard = (props) => {
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === "android") {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
+  return (
+    <Card style={{ ...styles.project, ...props.projectContainer }}>
+      <View style={styles.touchable}>
+        <Text style={{ ...styles.title, ...props.titleStyle }}>
+          {props.updateTitle}
+        </Text>
+        <Text style={{ ...styles.body, ...props.bodyStyle }}>
+          {props.updateBody}
+        </Text>
+      </View>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  project: {
+    height: 100,
+    borderWidth: 1,
+    width: "100%",
+  },
+  image: {
+    height: 70,
+    width: 70,
+    borderRadius: 70 / 2,
+    overflow: "hidden",
+  },
+  title: {
+    fontWeight: "500",
+    fontSize: 12,
+    margin: 20,
+    marginBottom: 10,
+  },
+  body: {
+    fontWeight: "300",
+    fontSize: 12,
+    marginHorizontal: 20,
+  },
+  touchable: {
+    overflow: "hidden",
+  },
+});
+
+export default UpdateCard;
---
+++ components/drawers/UpdatesHeader.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+
+const UpdatesHeader = (props) => {
+  let TouchableCmp = TouchableOpacity;
+  if (Platform.OS === "android") {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ ...styles.updatesTitle, ...props.updatesTitle }}>
+        Future Showcase Updates
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  updatesTitle: { margin: 20, fontSize: 26 },
+});
+
+export default UpdatesHeader;
