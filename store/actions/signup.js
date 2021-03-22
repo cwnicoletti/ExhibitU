dc1ef1ed189563596ffca56cd22962df92f8ca79
christiannicoletti <cwnicoletti@gmail.com>
2021-03-22 12:51:04 -0700

    Adding signup actions and reducer

---
+++ store/actions/signup.js
@@ -0,0 +1,49 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+export const INTROING = "INTROING";
+export const SIGNUP_EMAIL = "SIGNUP_EMAIL";
+export const SIGNUP_FULLNAME = "SIGNUP_FULLNAME";
+export const SIGNUP_USERNAME = "SIGNUP_USERNAME";
+export const SIGNUP_PASSWORD = "SIGNUP_PASSWORD";
+
+export const setIntroing = (localId, showcaseId, value) => {
+  return async (dispatch) => {
+    const userFeedGet = {
+      localId: localId,
+      showcaseId: showcaseId,
+      value: value,
+      switchName: "introing",
+    };
+
+    axios.post(
+      `https://us-central1-showcase-79c28.cloudfunctions.net/setSwitch`,
+      userFeedGet
+    );
+
+    AsyncStorage.getItem("userLoginData").then((data) => {
+      data = JSON.parse(data);
+      data.introing = value;
+      AsyncStorage.setItem("userLoginData", JSON.stringify(data));
+    });
+    await dispatch({ type: INTROING, introing: value });
+  };
+};
+
+export const setEmail = (value) => {
+  return async (dispatch) => {
+    await dispatch({ type: SIGNUP_EMAIL, email: value });
+  };
+};
+
+export const setFullname = (value) => {
+  return async (dispatch) => {
+    await dispatch({ type: SIGNUP_FULLNAME, fullname: value });
+  };
+};
+
+export const setUsername = (value) => {
+  return async (dispatch) => {
+    await dispatch({ type: SIGNUP_USERNAME, username: value });
+  };
+};
---
+++ store/reducers/signup.js
@@ -0,0 +1,41 @@
+import {
+  INTROING,
+  SIGNUP_EMAIL,
+  SIGNUP_FULLNAME,
+  SIGNUP_USERNAME,
+  SIGNUP_PASSWORD,
+} from "../actions/signup";
+
+const intialState = {
+  introing: false,
+  email: "",
+  fullname: "",
+  username: "",
+};
+
+export default (state = intialState, action) => {
+  switch (action.type) {
+    case INTROING:
+      return {
+        ...state,
+        introing: action.introing,
+      };
+    case SIGNUP_EMAIL:
+      return {
+        ...state,
+        email: action.email,
+      };
+    case SIGNUP_FULLNAME:
+      return {
+        ...state,
+        fullname: action.fullname,
+      };
+    case SIGNUP_USERNAME:
+      return {
+        ...state,
+        username: action.username,
+      };
+    default:
+      return state;
+  }
+};
