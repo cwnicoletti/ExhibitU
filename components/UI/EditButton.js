97a691ae16d23cd1a59d786bffb2d7b5388a4665
christiannicoletti <cwnicoletti@gmail.com>
2021-03-25 12:37:22 -0700

    Adding edit button component

---
+++ components/UI/EditButton.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+} from "react-native";
+import { useSelector } from "react-redux";
+
+const EditButton = (props) => {
+  const darkModeValue = useSelector((state) => state.switches.darkMode);
+
+  let TouchableCmp = TouchableOpacity;
+  if (Platform.OS === "android") {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+  return (
+    <TouchableCmp onPress={props.onPress}>
+      <View
+        style={{
+          marginTop: 10,
+          marginBottom: 5,
+          paddingHorizontal: "20%",
+          borderColor: darkModeValue ? "gray" : "#c9c9c9",
+          borderWidth: 1,
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            margin: 7,
+            color: darkModeValue ? "white" : "black",
+          }}
+        >
+          {props.editText}
+        </Text>
+      </View>
+    </TouchableCmp>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditButton;
