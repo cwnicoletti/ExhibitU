8c4a6e6e76c2491e0f404071ee8a6a2a97a36923
christiannicoletti <cwnicoletti@gmail.com>
2021-03-29 12:03:37 -0700

    Modifying card component

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -10,13 +10,6 @@ const Card = (props) => {
 const styles = StyleSheet.create({
   card: {
     shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowRadius: 8,
-    elevation: 5,
     backgroundColor: "white",
   },
 });
