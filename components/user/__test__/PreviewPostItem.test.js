08cd2f186764903744d1fc827e3b0424cdf117d3
christiannicoletti <cwnicoletti@gmail.com>
2021-03-29 12:11:37 -0700

    Adding tests to components/user (just starting)

---
+++ components/user/__test__/PreviewPostItem.test.js
@@ -0,0 +1,64 @@
+import React from "react";
+import PreviewPostItem from "../PreviewPostItem";
+import renderer from "react-test-renderer";
+
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import ReduxThunk from "redux-thunk";
+
+import authReducer from "../../../store/reducers/auth";
+import switchesReducer from "../../../store/reducers/switches";
+import userReducer from "../../../store/reducers/user";
+import signupReducer from "../../../store/reducers/signup";
+
+test("renders default", () => {
+  const rootReducer = combineReducers({
+    switches: switchesReducer,
+    signup: signupReducer,
+    auth: authReducer,
+    user: userReducer,
+  });
+
+  const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <PreviewPostItem />
+        </SafeAreaProvider>
+      </Provider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("renders with dummy links", () => {
+  const rootReducer = combineReducers({
+    switches: switchesReducer,
+    signup: signupReducer,
+    auth: authReducer,
+    user: userReducer,
+  });
+
+  const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+  const links = {
+    ["link1"]: {
+      linktitle1: "test",
+      linkUrl1: "test",
+    },
+  };
+
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <PreviewPostItem links={links} />
+        </SafeAreaProvider>
+      </Provider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ components/user/__test__/UserTitleEdit.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import UserTitleEdit from "../UserTitleEdit";
+import renderer from "react-test-renderer";
+
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import ReduxThunk from "redux-thunk";
+
+import authReducer from "../../../store/reducers/auth";
+import switchesReducer from "../../../store/reducers/switches";
+import userReducer from "../../../store/reducers/user";
+import signupReducer from "../../../store/reducers/signup";
+
+test("renders correctly", () => {
+  const rootReducer = combineReducers({
+    switches: switchesReducer,
+    signup: signupReducer,
+    auth: authReducer,
+    user: userReducer,
+  });
+
+  const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <UserTitleEdit />
+        </SafeAreaProvider>
+      </Provider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
