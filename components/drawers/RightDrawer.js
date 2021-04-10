adca6e935775725414682d8e8c93c7bf298da4d0
christiannicoletti <cwnicoletti@gmail.com>
2021-04-09 18:30:37 -0700

    Removing rightdrawer unnecessary update on darkmode update

--- components/drawers/RightDrawer.js
+++ components/drawers/RightDrawer.js
@@ -17,24 +17,6 @@ import { SimpleLineIcons } from "@expo/vector-icons";
 const RightDrawer = (props) => {
   const darkModeValue = useSelector((state) => state.switches.darkMode);
 
-  useEffect(() => {
-    props.component.navigationOptions = () => {
-      return {
-        tabBarOptions: {
-          activeTintColor: darkModeValue ? "white" : "black",
-          inactiveTintColor: darkModeValue ? "#696969" : "#bfbfbf",
-          tabStyle: {
-            backgroundColor: darkModeValue ? "black" : "white",
-          },
-          style: {
-            backgroundColor: darkModeValue ? "black" : "white",
-          },
-          showLabel: false,
-        },
-      };
-    };
-  }, [darkModeValue]);
-
   let TouchableCmp = TouchableOpacity;
   if (Platform.OS === "android") {
     TouchableCmp = TouchableNativeFeedback;
@@ -142,34 +124,6 @@ const RightDrawer = (props) => {
           </TouchableCmp>
         </View>
       </SafeAreaView>
-      {/* <SafeAreaView forceInset={{ top: "always", horizontal: "never" }}>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            margin: 10,
-          }}
-        >
-          <MaterialCommunityIcons
-            name="email-edit-outline"
-            size={20}
-            color={darkModeValue ? "white" : "black"}
-          />
-          <TouchableCmp onPress={props.feedbackOnPress}>
-            <View>
-              <Text
-                style={{
-                  color: darkModeValue ? "#bababa" : "black",
-                  fontSize: 18,
-                  marginHorizontal: 10,
-                }}
-              >
-                Direct Feedback
-              </Text>
-            </View>
-          </TouchableCmp>
-        </View>
-      </SafeAreaView> */}
       <View
         style={{
           flex: 1,
