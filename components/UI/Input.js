e44d921fb971399010354c89ede5705a6e4069cb
christiannicoletti <cwnicoletti@gmail.com>
2021-04-13 17:08:46 -0700

    Updating input.js with property shorthands, small fixes, and reducing complexity

--- components/UI/Input.js
+++ components/UI/Input.js
@@ -23,6 +23,32 @@ const inputReducer = (state, action) => {
   }
 };
 
+const textChangeHandler = (text) => {
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let isValid = true;
+  let error = props.errorText;
+  if (props.required && text.trim().length === 0) {
+    isValid = false;
+  }
+  if (props.email && !emailRegex.test(text.toLowerCase())) {
+    isValid = false;
+  }
+  if (props.min !== null && +text < props.min) {
+    isValid = false;
+  }
+  if (props.max !== null && +text > props.max) {
+    isValid = false;
+  }
+  if (props.confirmPassword !== null && props.confirmPassword !== text) {
+    isValid = false;
+    error = "Passwords don't match!";
+  }
+  if (props.minLength !== null && text.length < props.minLength) {
+    isValid = false;
+    error = "Password not long enough!";
+  }
+};
+
 const Input = (props) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: props.initialValue ? props.initialValue : "",
@@ -37,37 +63,12 @@ const Input = (props) => {
     onInputChange(id, inputState.value, inputState.isValid);
   }, [inputState, onInputChange, id]);
 
-  const textChangeHandler = (text) => {
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let isValid = true;
-    let error = props.errorText;
-    if (props.required && text.trim().length === 0) {
-      isValid = false;
-    }
-    if (props.email && !emailRegex.test(text.toLowerCase())) {
-      isValid = false;
-    }
-    if (props.min != null && +text < props.min) {
-      isValid = false;
-    }
-    if (props.max != null && +text > props.max) {
-      isValid = false;
-    }
-    if (props.confirmPassword != null && props.confirmPassword != text) {
-      isValid = false;
-      error = "Passwords don't match!";
-    }
-    if (props.minLength != null && text.length < props.minLength) {
-      isValid = false;
-      error = "Password not long enough!";
-    }
-    dispatch({
-      type: INPUT_CHANGE,
-      value: text,
-      isValid: isValid,
-      error: error,
-    });
-  };
+  dispatch({
+    type: INPUT_CHANGE,
+    value: text,
+    isValid,
+    error,
+  });
 
   const lostFocusHandler = () => {
     dispatch({
