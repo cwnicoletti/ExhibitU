8fe36b7464f4c3121fa92b39f7c96f9a9d865374
christiannicoletti <cwnicoletti@gmail.com>
2021-04-19 22:30:30 -0700

    Slight change SimpleLineIconsHeaderButton

--- components/UI/SimpleLineIconsHeaderButton.js
+++ components/UI/SimpleLineIconsHeaderButton.js
@@ -2,8 +2,10 @@ import React from "react";
 import { HeaderButton } from "react-navigation-header-buttons";
 import { SimpleLineIcons } from "@expo/vector-icons";
 
-const CustomHeaderButton = (props) => {
-  return <HeaderButton {...props} IconComponent={SimpleLineIcons} iconSize={23} />;
+const SimpleLineIconsHeaderButton = (props) => {
+  return (
+    <HeaderButton {...props} IconComponent={SimpleLineIcons} iconSize={23} />
+  );
 };
 
-export default CustomHeaderButton;
+export default SimpleLineIconsHeaderButton;
