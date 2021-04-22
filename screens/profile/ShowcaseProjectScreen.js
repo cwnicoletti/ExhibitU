180b4e279f035998d4444985bd2beae0181db3f8
christiannicoletti <cwnicoletti@gmail.com>
2021-04-22 15:17:56 -0700

    adding creatistId to showcaseprojectscreen

--- screens/profile/ShowcaseProjectScreen.js
+++ screens/profile/ShowcaseProjectScreen.js
@@ -16,6 +16,7 @@ import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 const ShowcaseProjectScreen = (props) => {
   const darkModeValue = useSelector((state) => state.switches.darkMode);
+  const creatistId = useSelector((state) => state.switches.creatistId);
   const currentProjectId = props.navigation.getParam("projectId");
   const profileProjects = props.navigation.getParam("profileProjects");
   const profilePictureBase64 =
