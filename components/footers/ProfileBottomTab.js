18902f5791da8d00982cd416055902974668e20e
christiannicoletti <cwnicoletti@gmail.com>
2021-04-23 18:52:41 -0700

    New Feature: Showcasing profile now hides bottom tabs

--- components/footers/ProfileBottomTab.js
+++ components/footers/ProfileBottomTab.js
@@ -1,108 +1,160 @@
-import React from "react";
-import { View, TouchableWithoutFeedback } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { View, TouchableWithoutFeedback, Animated } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
+import useDidMountEffect from "../helper/useDidMountEffect";
 
 import { Ionicons, SimpleLineIcons } from "@expo/vector-icons";
 
-import { resetScroll, onScreen } from "../../store/actions/user";
+import {
+  resetScroll,
+  onScreen,
+  setHideProfileFooter,
+} from "../../store/actions/user";
 
 const ProfileBottomTab = (props) => {
   const dispatch = useDispatch();
   const darkModeValue = useSelector((state) => state.switches.darkMode);
   const onProfileScreen = useSelector((state) => state.user.onProfileScreen);
+  const showcasingProfile = useSelector(
+    (state) => state.user.showcasingProfile
+  );
+  const [hiddenProfileFooter, setHiddenProfileFooter] = useState(false);
+
+  let slideAnim = useRef(new Animated.Value(0)).current;
+
+  const slideUp = () => {
+    Animated.timing(slideAnim, {
+      toValue: 0,
+      duration: 350,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const slideDown = () => {
+    Animated.timing(slideAnim, {
+      toValue: 100,
+      duration: 350,
+      useNativeDriver: true,
+    }).start(({ finished }) => {
+      if (finished) {
+        slideAnim.setValue(100);
+      }
+    });
+  };
+
+  useDidMountEffect(() => {
+    if (showcasingProfile === false) {
+      setHiddenProfileFooter(false);
+    }
+
+    if (showcasingProfile === false && hiddenProfileFooter === false) {
+      slideUp();
+    }
+  }, [hiddenProfileFooter]);
+
+  useEffect(() => {
+    if (showcasingProfile === true && hiddenProfileFooter === false) {
+      slideDown();
+      setHiddenProfileFooter(true);
+    }
+  }, []);
 
   return (
     <View>
-      <View style={{ flexDirection: "row" }}>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            props.navigation.navigate("Feed");
-          }}
-        >
+      {!hiddenProfileFooter ? (
+        <Animated.View style={{ transform: [{ translateY: slideAnim }] }}>
+          <View style={{ flexDirection: "row" }}>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                props.navigation.navigate("Feed");
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  padding: 20,
+                  borderTopWidth: 1,
+                  borderColor: "gray",
+                  backgroundColor: darkModeValue ? "black" : "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Ionicons
+                  name="ios-home"
+                  size={25}
+                  color={
+                    props.navigation.isFocused()
+                      ? "gray"
+                      : [darkModeValue ? "white" : "black"]
+                  }
+                />
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                props.navigation.navigate("Explore");
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  padding: 20,
+                  borderTopWidth: 1,
+                  borderColor: "gray",
+                  backgroundColor: darkModeValue ? "black" : "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Ionicons
+                  name="ios-search"
+                  size={25}
+                  color={
+                    props.navigation.isFocused()
+                      ? "gray"
+                      : [darkModeValue ? "white" : "black"]
+                  }
+                />
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                if (onProfileScreen) {
+                  dispatch(resetScroll("Profile"));
+                } else {
+                  dispatch(onScreen("Profile"));
+                }
+                props.navigation.navigate("Profile");
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  padding: 20,
+                  borderTopWidth: 1,
+                  borderColor: "gray",
+                  backgroundColor: darkModeValue ? "black" : "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <SimpleLineIcons
+                  name="trophy"
+                  size={25}
+                  color={darkModeValue ? "white" : "black"}
+                />
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
           <View
             style={{
-              flex: 1,
-              padding: 20,
-              borderTopWidth: 1,
-              borderColor: "gray",
+              padding: 10,
               backgroundColor: darkModeValue ? "black" : "white",
-              justifyContent: "center",
-              alignItems: "center",
             }}
-          >
-            <Ionicons
-              name="ios-home"
-              size={25}
-              color={
-                props.navigation.isFocused()
-                  ? "gray"
-                  : [darkModeValue ? "white" : "black"]
-              }
-            />
-          </View>
-        </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            props.navigation.navigate("Explore");
-          }}
-        >
-          <View
-            style={{
-              flex: 1,
-              padding: 20,
-              borderTopWidth: 1,
-              borderColor: "gray",
-              backgroundColor: darkModeValue ? "black" : "white",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Ionicons
-              name="ios-search"
-              size={25}
-              color={
-                props.navigation.isFocused()
-                  ? "gray"
-                  : [darkModeValue ? "white" : "black"]
-              }
-            />
-          </View>
-        </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            if (onProfileScreen) {
-              dispatch(resetScroll("Profile"));
-            } else {
-              dispatch(onScreen("Profile"));
-            }
-            props.navigation.navigate("Profile");
-          }}
-        >
-          <View
-            style={{
-              flex: 1,
-              padding: 20,
-              borderTopWidth: 1,
-              borderColor: "gray",
-              backgroundColor: darkModeValue ? "black" : "white",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <SimpleLineIcons
-              name="trophy"
-              size={25}
-              color={darkModeValue ? "white" : "black"}
-            />
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
-      <View
-        style={{
-          padding: 10,
-          backgroundColor: darkModeValue ? "black" : "white",
-        }}
-      />
+          />
+        </Animated.View>
+      ) : null}
     </View>
   );
 };
--- screens/profile/ShowcaseProfileScreen.js
+++ screens/profile/ShowcaseProfileScreen.js
@@ -1,91 +1,98 @@
 import React, { useEffect } from "react";
-import { Image, StyleSheet, FlatList, View, Text } from "react-native";
-import { useSelector } from "react-redux";
+import { StyleSheet, FlatList, View, Text } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import ProjectItem from "../../components/projectItems/ProfileProjectItem";
 import ShowcaseHeader from "../../components/user/ShowcaseHeader";
 import IoniconsHeaderButton from "../../components/UI/IoniconsHeaderButton";
 
+import { returnFromShowcasing } from "../../store/actions/user";
+
 const ShowcaseProfileScreen = (props) => {
+  const dispatch = useDispatch();
   const darkModeValue = useSelector((state) => state.switches.darkMode);
-  const DiamondCaseId = useSelector((state) => state.user.DiamondCaseId);
+  const ExhibitUId = useSelector((state) => state.user.ExhibitUId);
   const trueUndefined = void 0;
 
   const profilePictureBase64 =
-    props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+    props.navigation.getParam("ExhibitUId") === ExhibitUId ||
     props.navigation.getParam("profilePictureBase64") === trueUndefined
       ? useSelector((state) => state.user.profilePictureBase64)
       : props.navigation.getParam("profilePictureBase64");
 
   const profileColumns =
-    props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+    props.navigation.getParam("ExhibitUId") === ExhibitUId ||
     props.navigation.getParam("profileColumns") === trueUndefined
       ? useSelector((state) => state.user.profileColumns)
       : props.navigation.getParam("profileColumns");
 
   const userData = {
-    DiamondCaseId:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
-      props.navigation.getParam("DiamondCaseId") === trueUndefined
-        ? useSelector((state) => state.user.DiamondCaseId)
-        : props.navigation.getParam("DiamondCaseId"),
+    ExhibitUId:
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
+      props.navigation.getParam("ExhibitUId") === trueUndefined
+        ? useSelector((state) => state.user.ExhibitUId)
+        : props.navigation.getParam("ExhibitUId"),
     fullname:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("fullname") === trueUndefined
         ? useSelector((state) => state.user.fullname)
         : props.navigation.getParam("fullname"),
     username:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("username") === trueUndefined
         ? useSelector((state) => state.user.username)
         : props.navigation.getParam("username"),
     jobTitle:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("jobTitle") === trueUndefined
         ? useSelector((state) => state.user.jobTitle)
         : props.navigation.getParam("jobTitle"),
     profileBiography:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("profileBiography") === trueUndefined
         ? useSelector((state) => state.user.profileBiography)
         : props.navigation.getParam("profileBiography"),
     profileLinks:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("profileLinks") === trueUndefined
         ? useSelector((state) => state.user.profileLinks)
         : props.navigation.getParam("profileLinks"),
     profileProjects:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("profileProjects") === trueUndefined
         ? useSelector((state) => state.user.profileProjects)
         : props.navigation.getParam("profileProjects"),
     numberOfAdvocates:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("numberOfAdvocates") === trueUndefined
         ? useSelector((state) => state.user.numberOfAdvocates)
         : props.navigation.getParam("numberOfAdvocates"),
     numberOfFollowers:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("numberOfFollowers") === trueUndefined
         ? useSelector((state) => state.user.numberOfFollowers)
         : props.navigation.getParam("numberOfFollowers"),
     numberOfFollowing:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("numberOfFollowing") === trueUndefined
         ? useSelector((state) => state.user.numberOfFollowing)
         : props.navigation.getParam("numberOfFollowing"),
     showResumeValue:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("showResumeValue") === trueUndefined
         ? useSelector((state) => state.user.showResumeValue)
         : props.navigation.getParam("showResumeValue"),
     resumeLink:
-      props.navigation.getParam("DiamondCaseId") === DiamondCaseId ||
+      props.navigation.getParam("ExhibitUId") === ExhibitUId ||
       props.navigation.getParam("resumeLink") === trueUndefined
         ? useSelector((state) => state.user.resumeLink)
         : props.navigation.getParam("resumeLink"),
   };
+
+  useEffect(() => {
+    props.navigation.setParams({ dispatch: dispatch });
+  }, []);
 
   useEffect(() => {
     props.navigation.setParams({ darkMode: darkModeValue });
@@ -143,17 +150,17 @@ const ShowcaseProfileScreen = (props) => {
         onAddNewProjectPress={() => props.navigation.navigate("AddProject")}
         followersOnPress={() =>
           props.navigation.push("Followers", {
-            DiamondCaseId: userData.DiamondCaseId,
+            ExhibitUId: userData.ExhibitUId,
           })
         }
         followingOnPress={() =>
           props.navigation.push("Following", {
-            DiamondCaseId: userData.DiamondCaseId,
+            ExhibitUId: userData.ExhibitUId,
           })
         }
         advocatesOnPress={() =>
           props.navigation.push("Advocates", {
-            DiamondCaseId: userData.DiamondCaseId,
+            ExhibitUId: userData.ExhibitUId,
           })
         }
       />
@@ -195,27 +202,20 @@ const ShowcaseProfileScreen = (props) => {
 
 ShowcaseProfileScreen.navigationOptions = (navData) => {
   const darkModeValue = navData.navigation.getParam("darkMode");
+  const dispatch = navData.navigation.getParam("dispatch");
+
   return {
+    gestureEnabled: false,
     headerTitle: () => (
       <View style={styles.logo}>
-        {darkModeValue ? (
-          <Image
-            style={styles.image}
-            source={require("../../assets/DiamondCase_icon_transparent_white.png")}
-          />
-        ) : (
-          <Image
-            style={styles.image}
-            source={require("../../assets/DiamondCase_icon_transparent_black.png")}
-          />
-        )}
         <Text
           style={{
             ...styles.logoTitle,
             color: darkModeValue ? "white" : "black",
+            fontFamily: "CormorantUpright",
           }}
         >
-          Diamond Case
+          ExhibitU
         </Text>
       </View>
     ),
@@ -233,6 +233,7 @@ ShowcaseProfileScreen.navigationOptions = (navData) => {
           iconName={"ios-arrow-back"}
           color={darkModeValue ? "white" : "black"}
           onPress={() => {
+            dispatch(returnFromShowcasing());
             navData.navigation.goBack();
           }}
         />
@@ -245,6 +246,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: "space-between",
+    backgroundColor: "black",
   },
   profileTitleStyle: {
     fontSize: 24,
@@ -281,7 +283,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   logoTitle: {
-    fontSize: 22,
+    fontSize: 26,
   },
 });
 
