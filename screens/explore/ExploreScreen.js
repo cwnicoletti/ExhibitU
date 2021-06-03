b4336bdc32e91b5132ea84f788f325b7d6d866b4
christiannicoletti <cwnicoletti@gmail.com>
2021-06-02 18:32:45 -0700

    Adding Algolia logo, required for free version

--- screens/explore/ExploreScreen.js
+++ screens/explore/ExploreScreen.js
@@ -14,6 +14,7 @@ import { useDispatch, useSelector } from "react-redux";
 import ExploreCard from "../../components/explore/ExploreCard";
 import useDidMountEffect from "../../helper/useDidMountEffect";
 import { offScreen } from "../../store/actions/user";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const ExploreScreen = (props) => {
   const dispatch = useDispatch();
@@ -365,6 +366,25 @@ const ExploreScreen = (props) => {
           />
         )}
       />
+      <View
+        style={{
+          flexDirection: "row",
+          alignSelf: "center",
+          alignItems: "center",
+          margin: 10,
+        }}
+      >
+        <Text
+          style={{
+            color: "grey",
+            fontWeight: "bold",
+            marginRight: 10,
+          }}
+        >
+          Searches powered by Algolia
+        </Text>
+        <FontAwesome5 name="algolia" size={24} color={"grey"} />
+      </View>
     </View>
   );
 };
