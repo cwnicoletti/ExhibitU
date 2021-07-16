e9e927fce4c771dd951948832a4d70e0d1163244
christiannicoletti <cwnicoletti@gmail.com>
2021-07-16 14:45:43 -0700

    triggering loginUser action now gives authenticated res from back-end

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -96,148 +96,159 @@ export const login = (email, password) => {
   return async (dispatch) => {
     const loginForm = { email, password };
 
-    const getLoginResponse = await axios.post(
-      `https://us-central1-showcase-79c28.cloudfunctions.net/loginUser`,
-      loginForm
-    );
+    let authenticated = false;
+    let getLoginResponse = {};
 
-    const profilePictureBase64 = await getBase64FromUrl(
-      getLoginResponse.data.docData.profilePictureUrl
-    );
-
-    let profileProjects = await getLoginResponse.data.docData.profileProjects;
-
-    if (profileProjects) {
-      const projectKeys = Object.keys(profileProjects);
-      for (const k of projectKeys) {
-        const projectCoverPhotoBase64 = await getBase64FromUrl(
-          profileProjects[k]["projectCoverPhotoUrl"]
-        );
-        profileProjects[k]["projectCoverPhotoBase64"] = projectCoverPhotoBase64;
-        const postKeys = Object.keys(profileProjects[k].projectPosts);
-        for (const id of postKeys) {
-          const postPhotoBase64 = await getBase64FromUrl(
-            profileProjects[k].projectPosts[id]["postPhotoUrl"]
-          );
-          profileProjects[k].projectPosts[id]["postPhotoBase64"] =
-            postPhotoBase64;
-          profileProjects[k].projectPosts[id]["profilePictureBase64"] =
-            profilePictureBase64;
-        }
-      }
+    try {
+      getLoginResponse = await axios.post(
+        `https://us-central1-showcase-79c28.cloudfunctions.net/loginUser`,
+        loginForm
+      );
+      authenticated = true;
+    } catch (e) {
+      console.log(e);
     }
 
-    let userFeed = await getLoginResponse.data.docData.userFeed;
-    // if (userFeed) {
-    //   const feedKeys = Object.keys(userFeed);
-    //   for (const key of feedKeys) {
-    //     const postPhotoBase64 = await getBase64FromUrl(
-    //       userFeed[key]["postPhotoUrl"]
-    //     );
-    //     userFeed[key]["postPhotoBase64"] = postPhotoBase64;
-    //     if (
-    //       userFeed[key].ExhibitUId === getLoginResponse.data.docData.ExhibitUId
-    //     ) {
-    //       userFeed[key]["profilePictureBase64"] = profilePictureBase64;
-    //     } else {
-    //       userFeed[key]["profilePictureBase64"] = await getBase64FromUrl(
-    //         userFeed[key]["profilePictureUrl"]
-    //       );
-    //     }
-    //     const feedProjectKeys = Object.keys(userFeed[key].profileProjects);
-    //     for (const projectKey of feedProjectKeys) {
-    //       const postKeys = Object.keys(
-    //         userFeed[key].profileProjects[projectKey].projectPosts
-    //       );
-    //       const projectCoverPhotoBase64 = await getBase64FromUrl(
-    //         userFeed[key].profileProjects[projectKey]["projectCoverPhotoUrl"]
-    //       );
-    //       userFeed[key].profileProjects[projectKey]["projectCoverPhotoBase64"] =
-    //         projectCoverPhotoBase64;
-    //       for (const postKey of postKeys) {
-    //         const postPhotoBase64 = await getBase64FromUrl(
-    //           userFeed[key].profileProjects[projectKey].projectPosts[postKey][
-    //             "postPhotoUrl"
-    //           ]
-    //         );
-    //         userFeed[key].profileProjects[projectKey].projectPosts[postKey][
-    //           "postPhotoBase64"
-    //         ] = postPhotoBase64;
-    //         if (
-    //           userFeed[key].ExhibitUId ===
-    //           getLoginResponse.data.docData.ExhibitUId
-    //         ) {
-    //           userFeed[key].profileProjects[projectKey].projectPosts[postKey][
-    //             "profilePictureBase64"
-    //           ] = profilePictureBase64;
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
+    if (authenticated) {
+      const profilePictureBase64 = await getBase64FromUrl(
+        getLoginResponse.data.docData.profilePictureUrl
+      );
 
-    const projectTempCoverPhotoBase64 = await getBase64FromUrl(
-      getLoginResponse.data.docData.projectTempCoverPhotoUrl
-    );
+      let profileProjects = await getLoginResponse.data.docData.profileProjects;
 
-    const tempPhotoPostBase64 = await getBase64FromUrl(
-      getLoginResponse.data.docData.tempPhotoPostUrl
-    );
+      if (profileProjects) {
+        const projectKeys = Object.keys(profileProjects);
+        for (const k of projectKeys) {
+          const projectCoverPhotoBase64 = await getBase64FromUrl(
+            profileProjects[k]["projectCoverPhotoUrl"]
+          );
+          profileProjects[k]["projectCoverPhotoBase64"] =
+            projectCoverPhotoBase64;
+          const postKeys = Object.keys(profileProjects[k].projectPosts);
+          for (const id of postKeys) {
+            const postPhotoBase64 = await getBase64FromUrl(
+              profileProjects[k].projectPosts[id]["postPhotoUrl"]
+            );
+            profileProjects[k].projectPosts[id]["postPhotoBase64"] =
+              postPhotoBase64;
+            profileProjects[k].projectPosts[id]["profilePictureBase64"] =
+              profilePictureBase64;
+          }
+        }
+      }
 
-    await saveUserDocumentToStorage(
-      getLoginResponse.data.docData.ExhibitUId,
-      getLoginResponse.data.docData.email,
-      getLoginResponse.data.docData.profilePictureId,
-      getLoginResponse.data.docData.profilePictureUrl,
-      profilePictureBase64,
-      getLoginResponse.data.docData.projectTempCoverPhotoId,
-      getLoginResponse.data.docData.projectTempCoverPhotoUrl,
-      projectTempCoverPhotoBase64,
-      getLoginResponse.data.docData.tempPhotoPostId,
-      getLoginResponse.data.docData.tempPhotoPostUrl,
-      tempPhotoPostBase64,
-      getLoginResponse.data.docData.fullname,
-      getLoginResponse.data.docData.jobTitle,
-      getLoginResponse.data.docData.username,
-      getLoginResponse.data.docData.profileBiography,
-      getLoginResponse.data.docData.numberOfFollowers,
-      getLoginResponse.data.docData.numberOfFollowing,
-      getLoginResponse.data.docData.numberOfAdvocates,
-      getLoginResponse.data.docData.numberOfAdvocating,
-      getLoginResponse.data.docData.profileColumns,
-      getLoginResponse.data.docData.followers,
-      getLoginResponse.data.docData.following,
-      getLoginResponse.data.docData.advocates,
-      getLoginResponse.data.docData.advocating,
-      getLoginResponse.data.docData.projectsAdvocating,
-      getLoginResponse.data.docData.cheeredPosts,
-      profileProjects ? profileProjects : {},
-      getLoginResponse.data.docData.profileLinks
-        ? getLoginResponse.data.docData.profileLinks
-        : {},
-      userFeed ? userFeed : {},
-      getLoginResponse.data.docData.darkMode,
-      getLoginResponse.data.docData.showCheering,
-      getLoginResponse.data.docData.hideFollowing,
-      getLoginResponse.data.docData.hideFollowers,
-      getLoginResponse.data.docData.hideAdvocates,
-      getLoginResponse.data.docData.updates,
-      getLoginResponse.data.docData.tutorialing,
-      getLoginResponse.data.docData.tutorialPrompt,
-      getLoginResponse.data.docData.tutorialScreen
-    );
-    await saveDataToStorage(
-      getLoginResponse.data.localId,
-      getLoginResponse.data.idToken
-    );
+      let userFeed = await getLoginResponse.data.docData.userFeed;
+      // if (userFeed) {
+      //   const feedKeys = Object.keys(userFeed);
+      //   for (const key of feedKeys) {
+      //     const postPhotoBase64 = await getBase64FromUrl(
+      //       userFeed[key]["postPhotoUrl"]
+      //     );
+      //     userFeed[key]["postPhotoBase64"] = postPhotoBase64;
+      //     if (
+      //       userFeed[key].ExhibitUId === getLoginResponse.data.docData.ExhibitUId
+      //     ) {
+      //       userFeed[key]["profilePictureBase64"] = profilePictureBase64;
+      //     } else {
+      //       userFeed[key]["profilePictureBase64"] = await getBase64FromUrl(
+      //         userFeed[key]["profilePictureUrl"]
+      //       );
+      //     }
+      //     const feedProjectKeys = Object.keys(userFeed[key].profileProjects);
+      //     for (const projectKey of feedProjectKeys) {
+      //       const postKeys = Object.keys(
+      //         userFeed[key].profileProjects[projectKey].projectPosts
+      //       );
+      //       const projectCoverPhotoBase64 = await getBase64FromUrl(
+      //         userFeed[key].profileProjects[projectKey]["projectCoverPhotoUrl"]
+      //       );
+      //       userFeed[key].profileProjects[projectKey]["projectCoverPhotoBase64"] =
+      //         projectCoverPhotoBase64;
+      //       for (const postKey of postKeys) {
+      //         const postPhotoBase64 = await getBase64FromUrl(
+      //           userFeed[key].profileProjects[projectKey].projectPosts[postKey][
+      //             "postPhotoUrl"
+      //           ]
+      //         );
+      //         userFeed[key].profileProjects[projectKey].projectPosts[postKey][
+      //           "postPhotoBase64"
+      //         ] = postPhotoBase64;
+      //         if (
+      //           userFeed[key].ExhibitUId ===
+      //           getLoginResponse.data.docData.ExhibitUId
+      //         ) {
+      //           userFeed[key].profileProjects[projectKey].projectPosts[postKey][
+      //             "profilePictureBase64"
+      //           ] = profilePictureBase64;
+      //         }
+      //       }
+      //     }
+      //   }
+      // }
 
-    await dispatch(getUserData());
-    await dispatch(
-      await authenticate(
+      const projectTempCoverPhotoBase64 = await getBase64FromUrl(
+        getLoginResponse.data.docData.projectTempCoverPhotoUrl
+      );
+
+      const tempPhotoPostBase64 = await getBase64FromUrl(
+        getLoginResponse.data.docData.tempPhotoPostUrl
+      );
+
+      await saveUserDocumentToStorage(
+        getLoginResponse.data.docData.ExhibitUId,
+        getLoginResponse.data.docData.email,
+        getLoginResponse.data.docData.profilePictureId,
+        getLoginResponse.data.docData.profilePictureUrl,
+        profilePictureBase64,
+        getLoginResponse.data.docData.projectTempCoverPhotoId,
+        getLoginResponse.data.docData.projectTempCoverPhotoUrl,
+        projectTempCoverPhotoBase64,
+        getLoginResponse.data.docData.tempPhotoPostId,
+        getLoginResponse.data.docData.tempPhotoPostUrl,
+        tempPhotoPostBase64,
+        getLoginResponse.data.docData.fullname,
+        getLoginResponse.data.docData.jobTitle,
+        getLoginResponse.data.docData.username,
+        getLoginResponse.data.docData.profileBiography,
+        getLoginResponse.data.docData.numberOfFollowers,
+        getLoginResponse.data.docData.numberOfFollowing,
+        getLoginResponse.data.docData.numberOfAdvocates,
+        getLoginResponse.data.docData.numberOfAdvocating,
+        getLoginResponse.data.docData.profileColumns,
+        getLoginResponse.data.docData.followers,
+        getLoginResponse.data.docData.following,
+        getLoginResponse.data.docData.advocates,
+        getLoginResponse.data.docData.advocating,
+        getLoginResponse.data.docData.projectsAdvocating,
+        getLoginResponse.data.docData.cheeredPosts,
+        profileProjects ? profileProjects : {},
+        getLoginResponse.data.docData.profileLinks
+          ? getLoginResponse.data.docData.profileLinks
+          : {},
+        userFeed ? userFeed : {},
+        getLoginResponse.data.docData.darkMode,
+        getLoginResponse.data.docData.showCheering,
+        getLoginResponse.data.docData.hideFollowing,
+        getLoginResponse.data.docData.hideFollowers,
+        getLoginResponse.data.docData.hideAdvocates,
+        getLoginResponse.data.docData.updates,
+        getLoginResponse.data.docData.tutorialing,
+        getLoginResponse.data.docData.tutorialPrompt,
+        getLoginResponse.data.docData.tutorialScreen
+      );
+      await saveDataToStorage(
         getLoginResponse.data.localId,
         getLoginResponse.data.idToken
-      )
-    );
+      );
+      await dispatch(getUserData());
+      await dispatch(
+        await authenticate(
+          getLoginResponse.data.localId,
+          getLoginResponse.data.idToken
+        )
+      );
+      return authenticated;
+    }
   };
 };
 
