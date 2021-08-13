bd01b22ea393cef94a80fc16b28871426eba22d9
christiannicoletti <cwnicoletti@gmail.com>
2021-08-13 15:56:09 -0700

    Fixed photo permissions for android

--- helper/getPhotoPermissions.ts
+++ helper/getPhotoPermissions.ts
@@ -2,7 +2,7 @@ import { Platform, Alert, Linking } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 const getPhotoPermissions = async () => {
-  if (Platform.OS !== "web") {
+  if (Platform.OS === "ios") {
     const permissionResult =
       await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (permissionResult.granted !== true) {
@@ -20,6 +20,31 @@ const getPhotoPermissions = async () => {
           {
             text: "Cancel",
             style: "cancel",
+          },
+        ],
+        {
+          cancelable: true,
+          onDismiss: () =>
+            Alert.alert(
+              "This alert was dismissed by tapping outside of the alert dialog."
+            ),
+        }
+      );
+      return false;
+    } else {
+      return true;
+    }
+  } else if (Platform.OS === "android") {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted !== true) {
+      await Alert.alert(
+        "Permission Denied",
+        "Sorry, we need permission to access your photo library to make this work!",
+        [
+          {
+            text: "OK",
+            style: "default",
           },
         ],
         {
