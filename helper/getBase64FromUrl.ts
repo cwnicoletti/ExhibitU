36969d0918c88b7289829e8c0bf2de725b32051a
christiannicoletti <cwnicoletti@gmail.com>
2021-08-17 12:59:22 -0700

    getBase64FromUrl now its own component

---
+++ helper/getBase64FromUrl.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const getBase64FromUrl = async (url: string) => {
+  if (url) {
+    const response: any = await axios.get(url, {
+      responseType: "arraybuffer",
+    });
+    const base64: string = Buffer.from(await response.data, "base64").toString(
+      "base64"
+    );
+    return `data:image/png;base64,${base64}`;
+  } else {
+    return "";
+  }
+};
+
+export default getBase64FromUrl;
