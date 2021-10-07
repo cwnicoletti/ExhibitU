c1a7f645bd497267ce94e868953736824c927f51
christiannicoletti <cwnicoletti@gmail.com>
2021-10-06 17:07:03 -0700

    Slightly modifying NotificationCard component

--- components/notifications/NotificationCard.tsx
+++ components/notifications/NotificationCard.tsx
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { grey100 } from "react-native-paper/lib/typescript/styles/colors";
 import { AnimatedGradient } from "../custom/AnimatedGradient/AnimatedGradient";
 
 const NotificationCard = (props) => {
@@ -26,7 +27,7 @@ const NotificationCard = (props) => {
     <View style={{ ...styles.exhibit, ...props.exhibitContainer }}>
       <View style={styles.touchable}>
         <TouchableCmp onPress={props.onSelect} useForeground>
-          <View style={{ flexDirection: "row", margin: 10 }}>
+          <View style={{ flex: 1, flexDirection: "row", margin: 10 }}>
             <View style={{ ...props.imageContainer }}>
               {profileImageIsLoading ? (
                 <AnimatedGradient
@@ -34,9 +35,9 @@ const NotificationCard = (props) => {
                     top: 0,
                     position: "absolute",
                     zIndex: 3,
-                    height: 60,
-                    width: 60,
-                    borderRadius: 60 / 2,
+                    height: 40,
+                    width: 40,
+                    borderRadius: 40 / 2,
                   }}
                   colors={greyColorValues}
                   start={{ x: 0, y: 0 }}
@@ -60,23 +61,25 @@ const NotificationCard = (props) => {
             </View>
             <View style={{ ...styles.details, ...props.details }}>
               <Text style={{ ...styles.username, ...props.usernameStyle }}>
-                {props.username} is cheering on your post
+                {props.username} is cheering on your post!
               </Text>
             </View>
-            <Image
-              style={styles.postImage}
-              source={
-                props.postImage
-                  ? { uri: props.postImage }
-                  : require("../../assets/default-post-icon.png")
-              }
-              onLoadStart={() => {
-                setGreyColorValues(["rgba(0,0,0,1)", "rgba(50,50,50,1)"]);
-              }}
-              onLoadEnd={() => {
-                setProfileImageIsLoading(false);
-              }}
-            />
+            <View style={{ flex: 1, alignItems: "flex-end" }}>
+              <Image
+                style={styles.postImage}
+                source={
+                  props.postImage
+                    ? { uri: props.postImage }
+                    : require("../../assets/default-post-icon.png")
+                }
+                onLoadStart={() => {
+                  setGreyColorValues(["rgba(0,0,0,1)", "rgba(50,50,50,1)"]);
+                }}
+                onLoadEnd={() => {
+                  setProfileImageIsLoading(false);
+                }}
+              />
+            </View>
           </View>
         </TouchableCmp>
       </View>
@@ -89,21 +92,23 @@ const styles = StyleSheet.create({
     marginVertical: 2,
     width: "100%",
     justifyContent: "center",
+    borderBottomWidth: 1,
+    borderColor: "grey",
   },
   image: {
-    height: 60,
-    width: 60,
-    borderRadius: 60 / 2,
+    height: 40,
+    width: 40,
+    borderRadius: 40 / 2,
     overflow: "hidden",
   },
   postImage: {
-    height: 60,
-    width: 60,
-    borderRadius: 60 / 2,
+    height: 40,
+    width: 40,
+    borderRadius: 40 / 2,
     overflow: "hidden",
   },
   username: {
-    fontWeight: "800",
+    fontWeight: "500",
     fontSize: 14,
   },
   details: {
