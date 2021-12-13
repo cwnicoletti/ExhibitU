7a73962d5ee84e37ab15646d8e508b0b0d3a6a99
christiannicoletti <cwnicoletti@gmail.com>
2021-12-13 13:26:54 -0800

    adding to jest.config.js "transformIgnorePatterns"

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const esModules =
-  "react-navigation-header-buttons|@expo|react-native-adapter|expo-linear-gradient|@unimodules|react-native|react-native-gesture-handler|vector-icons|expo-font|@unimodules|expo-asset|expo-constants|expo-file-system|expo-web-browser|react-navigation|react-navigation-stack|unimodules-permissions-interface|expo-permissions|expo-image-picker|expo-linear-gradient|expo-constants";
+  "react-navigation-header-buttons|react|@expo|expo-modules-core|@react-native|react-native-adapter|expo-linear-gradient|@unimodules|react-native|react-native-gesture-handler|vector-icons|expo-font|@unimodules|expo-asset|expo-constants|expo-file-system|expo-web-browser|react-navigation|react-navigation-stack|unimodules-permissions-interface|expo-permissions|expo-image-picker|expo-linear-gradient|expo-constants";
 
 module.exports = {
   preset: "jest-expo",
